80e38ed78d57bbfb192cc791f1808ea8a8984917
AzureUmbra <geeteepee13@gmail.com>
2019-10-05 02:39:19 -0500

    rc testing

--- testing.py
+++ testing.py
@@ -3,11 +3,21 @@ from time import sleep
 
 
 if __name__ == '__main__':
+    ip = '192.168.1.107'
     c = TelloUDP()
     c.start()
-    x = c.sendCommand('127.0.0.1','this is a test of a command')
-    print('--{}'.format(x))
-    for i in range(15):
-        sleep(1)
-        print(c.getData())
+    c.sendCommand(ip,'command')
+    sleep(1)
+    c.sendCommand(ip,'takeoff')
+    sleep(5)
+    for i in range(0,50):
+        c.sendCommand(ip,'rc 0 {} 0 0'.format(i))
+        print(c.getCommand())
+    for i in range(100,-51,-1):
+        c.sendCommand(ip,'rc 0 {} 0 0'.format(i))
+        print(c.getCommand())
+    for i in range(-49,1):
+        c.sendCommand(ip,'rc 0 {} 0 0'.format(i))
+        print(c.getCommand())
+    c.sendCommand(ip,'land')
     c.stop()
